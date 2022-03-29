82d001a881cd9120ed851b20b0149a5db6df5ff8
erik schievenin <erik.schievenin@gmail.com>
2022-03-29 09:24:03 +0200

    Exercise complete

--- js/script.js
+++ js/script.js
@@ -28,12 +28,85 @@ const slides = [
 
 console.log(slides);
 
+// Partendo dal template fornito renderizzare lo slider con Vue
+// Bonus:
+// 1- al click su una thumb, visualizzare in grande l'immagine corrispondente
+// 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+// 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
+
 const app = new Vue({
     el: '#app',
     data: {
         slides,
         activeSlideIndex: 0,
+        intervalCleanerId: undefined
+    },
 
+    methods: {
+        showPrevSlide() {
+            if (this.activeSlideIndex > 0) {
+                this.activeSlideIndex--;
+            } else {
+                this.activeSlideIndex = this.slides.length - 1;
+            }
+        },
+
+        showNextSlide() {
+            if (this.activeSlideIndex < this.slides.length - 1) {
+                this.activeSlideIndex++;
+            } else {
+                this.activeSlideIndex = 0;
+            }
+        },
+
+        checkIfActive(item) {
+            console.log(item.title)
+
+            /*const index = this.slides.findIndex(
+                (slide) => {
+                    return slide.title = item.title;
+                }
+            )
+
+            if (index === this.activeSlideIndex) {
+                return 'thumb active';
+            }
+            return 'thumb';*/
+
+        },
+
+        selectSlide(item) {
+
+            // indice numerico che indica la posizione dell'elemento
+            // all'interno dell'array
+
+            this.activeSlideIndex = item;
+
+
+            // Se non ci fose l'index saremmo costretti a iterare 
+            // tutto l'array alla ricerca dell'elemento(cliccato in questo caso)
+            // per essere certi di recuperare l'indice corretto, gli elementi non dovrebbero essere duplicati/simili
+
+            // const index = this.slides.findIndex(
+            //     (slide) => {
+            //         return slide.title === item.title;
+            //     }
+            // )
+            // if (index > -1) {
+            //     this.activeSlideIndex = index;
+            // }
+        },
+
+        startTimer() {
+            this.intervalCleanerId = setInterval(this.showNextSlide, 3000);
+
+            console.log('this.intervalCleanerId', this.intervalCleanerId);
+        },
+
+        stopTimer() {
+            clearInterval(this.intervalCleanerId);
+            console.log('clear interval');
+        }
     }
 
-})
+});
